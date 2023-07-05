090e9fae001a0a2c70ee8eb39cdbd1936a7228be
manjil <manjilrabhandari@gmail.com>
2023-07-05 11:18:48 +0545

    git init

---
+++ Weather_App/AppBase/BaseController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseController.swift
+//  Weather_App
+//
+//  Created by manjil on 05/07/2023.
+//
+
+import UIKit
+
+class BaseController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
